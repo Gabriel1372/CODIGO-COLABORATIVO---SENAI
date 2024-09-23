a6dbad2770f3f0221961d3f41ddfc83f6209d638
Gabriel1372 <gabrielsabtiago027@gmail.com>
2024-09-23 18:38:29 +0000

    salvar atividade II

--- Atividade_1.py
+++ Atividade_1.py
@@ -1,6 +1,5 @@
 import os
 os.system("cls || clear")
-
 print("Atividade - I")
 
 print("Feito por:colaborador 1")
--- Atividade_3.py
+++ Atividade_3.py
@@ -1,6 +1,6 @@
-import os
-os.system("cls || clear")
 
 print("Atividade - I")
+import os
+os.system("cls || clear")
 
 print("Feito por:colaborador 1")
